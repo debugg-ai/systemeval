657d6f0a728745f4ac20b0d9a93554e62c21ff6f
qosha1 <quinnosha@gmail.com>
2026-01-26 15:14:58 -0800

    Fix git_analyzer backward compat shim - export functions not classes

--- systemeval-py/systemeval/e2e/git_analyzer.py
+++ systemeval-py/systemeval/e2e/git_analyzer.py
@@ -3,22 +3,38 @@ Backward compatibility shim for e2e git_analyzer.
 
 After Phase 2 reorganization, git_analyzer moved to e2e/analysis/git_analyzer.py
 This file provides backward compatibility for imports like:
-    from systemeval.e2e.git_analyzer import GitAnalyzer
+    from systemeval.e2e.git_analyzer import analyze_commit
 
-Prefer importing from systemeval.e2e.analysis:
-    from systemeval.e2e.analysis import GitAnalyzer
+Prefer importing from systemeval.e2e.analysis.git_analyzer:
+    from systemeval.e2e.analysis.git_analyzer import analyze_commit
 """
 
 from .analysis.git_analyzer import (
-    GitAnalyzer,
-    RepositoryState,
-    FileChange,
-    parse_git_diff,
+    GitAnalysisError,
+    get_current_branch,
+    get_default_branch,
+    analyze_working_changes,
+    analyze_commit,
+    analyze_range,
+    analyze_pr_changes,
+    _parse_change_type,
+    _parse_numstat_line,
+    _parse_diff_output,
+    _run_git_command,
+    _validate_repo,
 )
 
 __all__ = [
-    "GitAnalyzer",
-    "RepositoryState",
-    "FileChange",
-    "parse_git_diff",
+    "GitAnalysisError",
+    "get_current_branch",
+    "get_default_branch",
+    "analyze_working_changes",
+    "analyze_commit",
+    "analyze_range",
+    "analyze_pr_changes",
+    "_parse_change_type",
+    "_parse_numstat_line",
+    "_parse_diff_output",
+    "_run_git_command",
+    "_validate_repo",
 ]
